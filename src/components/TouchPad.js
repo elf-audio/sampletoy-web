773e24f68627b35bcdc970276664e0110b6f7bd2
Marek-mba <bereza@gmail.com>
2023-04-13 20:57:21 +1000

    dynamic svg and canvas started

--- src/components/TouchPad.js
+++ src/components/TouchPad.js
@@ -1,9 +1,25 @@
 import star from '../star.svg'
-const TouchPad = () => {
-	return <div id="chop" className="inner-shadow">
-			<div className="touch"><img src={star} /></div>
 
-			</div>
+const TouchPad = (props) => {
+	
+	
+	
+	
+	
+	let shapes = []
+	for(let i = 0; i < 10; i++) {
+		shapes.push(<circle cx={10*i} cy={50} r={5} fill="red" />)
+	}
+	return <div id="chop" className="inner-shadow">
+	
+	<svg>
+		{shapes}
+      </svg>
+	{/* <div className="touch"><img src={star} /></div> */}
+	
+	</div>
 }
 
 export default TouchPad
+
+
--- src/components/WaveCropper.js
+++ src/components/WaveCropper.js
@@ -1,5 +1,27 @@
-const WaveCropper = () => {
-    return <div id="wave"><div className="overshadow"></div></div>
+import React, { useRef, useEffect } from 'react'
+
+
+const WaveCropper = (props) => {
+
+    const canvasRef = useRef(null)
+
+    const draw = (ctx) => {
+		ctx.fillStyle = '#000000'
+		ctx.beginPath()
+		ctx.arc(50, 100, 20, 0, 2*Math.PI)
+		ctx.fill()
+	}
+
+    useEffect(() => {
+		const canvas = canvasRef.current
+		const context = canvas.getContext('2d')
+		//Our first draw
+		draw(context)
+	}, [draw])
+
+    return <div id="wave"><div className="overshadow">
+        <canvas ref={canvasRef} {...props}/>
+        </div></div>
 }
 
 export default WaveCropper
