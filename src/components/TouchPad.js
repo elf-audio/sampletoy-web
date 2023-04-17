09e930d9fabc8c50780993d86560f0f593907ef1
Marek-mba <bereza@gmail.com>
2023-04-17 12:34:08 +1000

    fixed grid drawing to fill component, slight refactor

--- src/components/TouchPad.js
+++ src/components/TouchPad.js
@@ -9,29 +9,23 @@ import FullSizeSVG from './FullSizeSVG'; // Import the FullSizeSVG component
 const TouchPad = (props) => {
 	const createSVGContent = (width, height) => {
 		
-		
-		
-		const scaleX = Math.min(window.innerWidth, window.innerHeight)
-		const scaleY = scaleX
-		
+		const scale = Math.min(window.innerWidth, window.innerHeight)
+
 		
 		const numCols = 50
 		
-		
 		const rows = 10;
 		const cols = 10;
-		const cellWidth = width / cols;
-		const cellHeight = height / rows;
 		
 		let grid = '';
 		
 		// Draw horizontal lines
-		for (let i = 1; i < numCols; i++) {
+		for (let i = 1; i < numCols*20; i++) {
 			let a = i / numCols
 			let alpha = 0.1
 			if(i%2==0) alpha = 0.3
 			if(i%10==0) alpha = 0.6
-			let yy = Math.floor(a*scaleY)
+			let yy = Math.floor(a*scale)
 			const col = "rgba(172,255,182,"+alpha+")"
 			grid += `
 			<line key="${i+10000}" x1="0" y1="${yy}" x2="${width}" y2="${yy}" stroke="${col}" />"
@@ -41,18 +35,18 @@ const TouchPad = (props) => {
 		
 		
 		// Draw vertical lines
-		for(let i = 1; i < numCols; i++) {
+		for(let i = 1; i < numCols*20; i++) {
 			let a = i /numCols;
 			let alpha = 0.2;
 			if(i%2==0) alpha = 0.4;
 			if(i%10==0) alpha = 0.8;
-			const xx = Math.floor(a*scaleX)
+			const xx = Math.floor(a*scale)
 			
 			const col = "rgba(172,255,182,"+alpha+")"
 			
 			grid += `<line x1="${xx}" y1="0" x2="${xx}" y2="${height}" stroke="${col}" />`
 			
-			if(a*scaleX>width-5) break;
+			if(a*scale>width-5) break;
 		}
 		
 		
